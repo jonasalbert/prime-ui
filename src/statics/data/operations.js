9f095cde14790031dd3496f609bf84a47c17fd2d
jonasalbert <Jonasalbert@gmail.com>
2019-12-20 03:54:34 +0800

    update static data

--- src/statics/data/operations.js
+++ src/statics/data/operations.js
@@ -1,11 +1,11 @@
 [
   { "id": 1, "name": "Approved", "prime_number": 2 },
-  { "id": 2, "name": "Disapproved", "prime_number": 2 },
-  { "id": 3, "name": "Add", "prime_number": 2 },
-  { "id": 4, "name": "Update", "prime_number": 2 },
-  { "id": 5, "name": "Delete", "prime_number": 2 },
-  { "id": 6, "name": "Void", "prime_number": 2 },
-  { "id": 7, "name": "Draft", "prime_number": 2 },
-  { "id": 8, "name": "Terminated", "prime_number": 2 },
-  { "id": 9, "name": "Processed", "prime_number": 2 },
+  { "id": 2, "name": "Disapproved", "prime_number": 3 },
+  { "id": 3, "name": "Add", "prime_number": 5 },
+  { "id": 4, "name": "Update", "prime_number": 7 },
+  { "id": 5, "name": "Delete", "prime_number": 11 },
+  { "id": 6, "name": "View", "prime_number": 13 },
+  { "id": 7, "name": "Draft", "prime_number": 17 },
+  { "id": 8, "name": "Terminated", "prime_number": 19 },
+  { "id": 9, "name": "Processed", "prime_number": 23 }
 ]
---
+++ src/statics/data/permission_users.js
@@ -0,0 +1,12 @@
+[
+  { "id": 1, "users_id": 1, "resources_id": 1, "prime_formula_operations": 100},
+  { "id": 2, "users_id": 1, "resources_id": 2, "prime_formula_operations": 100},
+  { "id": 3, "users_id": 1, "resources_id": 3, "prime_formula_operations": 100},
+  { "id": 4, "users_id": 1, "resources_id": 4, "prime_formula_operations": 100},
+  { "id": 5, "users_id": 1, "resources_id": 5, "prime_formula_operations": 100},
+  { "id": 6, "users_id": 2, "resources_id": 1, "prime_formula_operations": 100},
+  { "id": 7, "users_id": 2, "resources_id": 2, "prime_formula_operations": 100},
+  { "id": 8, "users_id": 3, "resources_id": 5, "prime_formula_operations": 13},
+  { "id": 9, "users_id": 4, "resources_id": 5, "prime_formula_operations": 13},
+  { "id": 10, "users_id": 5, "resources_id": 5, "prime_formula_operations": 13}
+]
