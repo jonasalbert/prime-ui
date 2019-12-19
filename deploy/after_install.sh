a1a50cf59ad58650558f896a72357db4bf741fc1
jonasalbert <Jonasalbert@gmail.com>
2019-12-19 09:59:47 +0800

    build the project

---
+++ deploy/after_install.sh
@@ -0,0 +1,4 @@
+sudo chmod -R 775 /var/www/html/prime_ui_source
+# sudo rm -rf /var/www/html/app_source/node_modules
+sudo ln -sfn /var/www/html/prime_ui_source/dist/pwa /var/www/html/prime_ui_source
+
